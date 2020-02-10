311b100949cbd45ddb324058a218509ba26c780f
andre.garcia <andre.garcia@gatec.com.br>
2020-02-10 16:19:21 -0300

    adicionando arquivos de exercícios .net

---
+++ AtividadeWiki1/AtividadeWiki1/Program.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+
+/*I.Construa um programa Console Application em C# que leia uma operação dentre as possíveis operações:
+
++ soma
+- subtração
+* multiplicação
+/ divisão
+^ potência
+% resto de divisão
+.Sair do programa
+Caso o usuário informe a operação.encerre o programa.Caso ele informe uma das demais operações, leia valores no formato decimal e realize 
+tal operação informada.
+
+Considerações:
+
+Implemente regras para aceitar apenas as operações listadas acima e caso não seja uma delas apresente um erro.
+Implemente uma tratativa para aceitar apenas números nos valores (decimal) e ao digitar letras ou texto apresente um erro.
+Evolua o programa para um loop infinito encerrando o loop apenas quando o usuário digitar.para a operação.Limpe a tela a cada nova operação.
+Dicas:
+Utilize blocos try/catch para tratativas e loops para insistir até que o usuário informe um valor válido.*/
+
+namespace AtividadeWiki1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var expression = "";
+
+            while (true)
+            {
+                Console.WriteLine("Digite uma expressão matematica no formato {número [+] número} (Digite enter quando terminar):\n");
+
+                expression = Console.ReadLine();
+
+                if (expression == ".")
+                    return;
+
+                try
+                {
+                    if (Validation(expression))
+                    {
+                        var result = new Operators(expression).ReadExpression();
+                        Console.WriteLine(result.ToString());
+                    }
+                    else
+                        Console.WriteLine("Expressão inválida");
+                    
+                } catch(Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+                Console.ReadLine();
+                Console.Clear();
+            }
+        }
+
+        static public bool Validation(string expression)
+        {
+            var args = expression.Split(" ");
+            List<string> operators = new List<string> { "+", "-", "*", "/", "^", "%" };
+
+            try
+            {
+                decimal op1 = Convert.ToDecimal(args[0].Trim());
+                decimal op2 = Convert.ToDecimal(args[2].Trim());
+            }
+            catch (Exception)
+            {
+                throw new System.ArgumentException("O primeiro ou o segundo parâmetro da expressão não é decimal.");
+            }
+
+            if (!operators.Exists (x => x==args[1]))
+                throw new System.ArgumentException("A operação matemática utilizada não é válida. Utilize '+', '-', '*', '/', '^', '%'");
+            
+            return true;
+        }
+    }
+
+    class Operators
+    {
+        public Operators()
+        {
+
+        }
+
+        public Operators(string expression)
+        {
+            this.Expression = expression;
+        }
+
+        private Operators(decimal value)
+        {
+            this.Value = value;
+        }
+
+        public decimal Value { get; set; }
+        public decimal? Result { get; set; }
+        public string Expression { get; set; }
+
+        public Operators ReadExpression()
+        {
+            string[] args;
+
+            try
+            {
+                args = this.Expression.Split(" ");
+                Operators op1 = new Operators(Convert.ToDecimal(args[0]));
+                Operators op2 = new Operators(Convert.ToDecimal(args[2]));
+
+                if (args[1] == "+")
+                    return op1 + op2;
+
+                if (args[1] == "-")
+                    return op1 - op2;
+
+                if (args[1] == "*")
+                    return op1 * op2;
+
+                if (args[1] == "/")
+                    return op1 / op2;
+
+                if (args[1] == "^")
+                    return op1 ^ op2;
+
+                if (args[1] == "%")
+                    return op1 % op2;
+            }
+            catch(Exception)
+            {
+                throw new System.ArgumentException("Operação inválida");
+            }
+            
+            return null;
+        }
+
+        public static Operators operator +(Operators op1, Operators op2)
+        {
+            Operators op = new Operators();
+            op.Result = op1.Value + op2.Value;
+            return op;
+        }
+
+        public static Operators operator -(Operators op1, Operators op2)
+        {
+            Operators op = new Operators();
+            op.Result = op1.Value - op2.Value;
+            return op;
+        }
+
+        public static Operators operator *(Operators op1, Operators op2)
+        {
+            Operators op = new Operators();
+            op.Result = op1.Value * op2.Value;
+            return op;
+        }
+
+        public static Operators operator /(Operators op1, Operators op2)
+        {
+            Operators op = new Operators();
+            op.Result = op1.Value / op2.Value;
+            return op;
+        }
+
+        public static Operators operator ^(Operators op1, Operators op2)
+        {
+            Operators op = new Operators();
+            op.Result = (decimal)Math.Pow((double)op1.Value, (double)op2.Value);
+            return op;
+        }
+
+        public static Operators operator %(Operators op1, Operators op2)
+        {
+            Operators op = new Operators();
+            op.Result = op1.Value % op2.Value;
+            return op;
+        }
+
+        public override string ToString()
+        {
+            return $"= { this.Result }";
+        }
+
+    }
+}
---
+++ AtividadeWiki2/AtividadeWiki2/Program.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+/*
+II.Construa um programa Console Application em C# que defina um modelo de ordem de serviço. Considere as informações para OS:
+Número(auto incremento)
+Data de Abertura
+Data de Encerramento(pode aceitar nulo)
+Nome do Responsável
+Áreas da OS(soma das áreas)
+
+Uma área pode ser composta por:
+Código
+Tamanho da Área
+
+Utilizando uma List<T>(como um banco de dados em memória), o usuário poderá operar as seguintes informações:
+
+0. Sair do programa.
+1. Criar uma nova OS(solicitar data de abertura).
+2. Listar todas as OS cadastradas.
+3. Encerrar uma OS.
+4. Incluir uma nova área na OS.
+Dicas: Construa duas classes uma para a OS e outra para a Área e relacione estas através de propriedades (1 para N). 
+Solicite as informações e utilizando uma List<T> para simular um banco de dados em memória, realize as operações listadas.
+Crie um loop sobre o menu para que o programa não encerre com apenas uma operação.
+*/
+
+namespace AtividadeWiki2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<OrdemDeServico> ListaDeOS = new List<OrdemDeServico>();
+            int comando = -1;
+
+            while (true)
+            {
+                InicializarMenu();
+
+                comando = ReceberComando();
+
+                switch (comando)
+                {
+                    case 0:
+                        return;
+                        break;
+
+                    case 1:
+
+                        Console.Clear();
+                        Console.Write("Digite a data da abertura da OS ou 0 para retornar ao Menu Principal: ");
+                        comando = ReceberComando();
+
+                        if (!comando.Equals(0))
+                        {
+                            OrdemDeServico os = new OrdemDeServico();
+                            while (RealizaAberturaDaOS(os, ListaDeOS)) { }
+                        }
+
+                        break;
+
+                    default:
+                        break;
+
+
+                }
+            }
+        }
+
+        static bool RealizaAberturaDaOS(OrdemDeServico os, List<OrdemDeServico> lista)
+        {
+            try
+            {
+                if (lista.Count > 0)
+                    os.Numero = lista.Max(x => x.Numero) + 1;
+                else
+                    os.Numero = 1;
+
+                os.Abertura = Convert.ToDateTime(Console.ReadLine());
+                os.Responsavel = Console.ReadLine();
+
+                Console.Write("\nOS criada com sucesso. Pressione qualquer tecla para continuar...\n");
+                Console.ReadLine();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.Write("\nOcorreu um erro na abertura da OS.\n" + ex.Message);
+                Console.ReadLine();
+                return false;
+            }
+
+        }
+
+        static int ReceberComando()
+        {
+            try
+            {
+                var comando = Convert.ToInt32(Console.ReadLine());
+                return comando;
+            }
+            catch (Exception)
+            {
+                Console.Write("\nOpção inválida.\n");
+            }
+
+            return -1;
+        }
+
+        static void InicializarMenu()
+        {
+            Console.Clear();
+            Console.Write("0. Sair do programa.\n" +
+                          "1. Criar uma nova OS.\n" +
+                          "2. Listar todas as OS cadastradas.\n" +
+                          "3. Encerrar uma OS.\n" +
+                          "4. Incluir uma nova área na OS.\n\n");
+
+            Console.Write("Digite sua opção: ");
+        }
+    }
+
+    public class OrdemDeServico
+    {
+        public OrdemDeServico()
+        {
+
+        }
+
+        public long Numero { get; set; }
+        public DateTime Abertura { get; set; }
+        public DateTime? Encerramento { get; set; }
+        public string Responsavel { get; set; }
+        public List<Area> Areas { get; set; }
+
+    }
+
+    public class Area
+    {
+        public Area()
+        {
+
+        }
+
+        public long Codigo { get; set; }
+        public decimal Tamanho { get; set; }
+
+    }
+
+    public static class Auxiliar
+    {
+        static long ProximoID(this List<OrdemDeServico> lista)
+        {
+            return lista.Max(x => x.Numero) + 1;
+        }
+    }
+
+}
